ef829e6645844cfd05a4ab95c4dec32b4ee6581f
i-naeem <inaeem@protonmail.com>
2022-04-22 10:25:09 +0500

    :goal_net: add validations

--- routes/encoder.js
+++ routes/encoder.js
@@ -4,10 +4,19 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   const { data } = req.query;
-  const dataBits = data.split('').map((bit) => parseInt(bit));
-  const hammingCode = await HammingCode.encode(dataBits);
 
-  return res.json(hammingCode);
+  try {
+    if (data.search(/[^10]+/g) > -1) {
+      return res.status(422).json({
+        error: 'Invalid data, data must not contain anything other than 1 or 0',
+      });
+    }
+
+    const hammingCode = await HammingCode.encode(dataBits);
+    return res.json(hammingCode);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
