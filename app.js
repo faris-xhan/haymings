fad0687ecd380272576463fdf6fdfe7565b22961
i-naeem <inaeem@protonmail.com>
2022-04-22 09:50:47 +0500

    :tada: Initialize & setup project

---
+++ app.js
@@ -0,0 +1,18 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+const indexRouter = require('./routes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
