2f99534f424776610a7796143a9d9de71a89eebe
i-naeem <inaeem@protonmail.com>
2022-04-22 10:40:12 +0500

    :fire: add loggin and helmet

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const logger = require('morgan');
+const helmet = require('helmet');
 const path = require('path');
+const fs = require('fs');
 
 const indexRouter = require('./routes');
 const encoderRouter = require('./routes/encoder');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(helmet());
+app.use(
+  logger('combined', {
+    stream: fs.createWriteStream('./logs/access.log', { flags: 'a' }),
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
